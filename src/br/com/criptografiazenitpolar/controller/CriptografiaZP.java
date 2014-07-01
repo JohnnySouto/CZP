e67b91c9c79758dab0a84d5523fbbcfec9f2d8f6
Johnny Souto <johnnyjns@gmail.com>
2014-07-01 17:41:49 -0300

    implementando a interface

---
+++ src/br/com/criptografiazenitpolar/controller/CriptografiaZP.java
@@ -0,0 +1,21 @@
+package br.com.criptografiazenitpolar.controller;
+
+
+/**
+ *
+ * @author Johnny Souto e-mail: falecomigo@johnnysouto.com.br
+ */
+
+public class CriptografiaZP implements ICriptografiaZP{
+
+    @Override
+    public String CriptografarMensagem() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String DesCriptografarMensagem() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
