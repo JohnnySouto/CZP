ed0f3b5d8e05cfc98a2dd0c8684aae168f84c2b5
Johnny Souto <johnnyjns@gmail.com>
2014-07-01 15:47:43 -0300

    Incluindo interface

---
+++ src/br/com/criptografiazenitpolar/controller/ICriptografiaZP.java
@@ -0,0 +1,14 @@
+package br.com.criptografiazenitpolar.controller;
+
+/**
+ *
+ * @author Johnny Souto e-mail: falecomigo@johnnysouto.com.br
+ */
+
+public interface ICriptografiaZP {
+    
+    public String getCriptografar();
+    
+    public String getDesCriptografar();
+    
+}
